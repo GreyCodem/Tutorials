a144c422d75fbf7d8148930b68aa7ab81a424d65
GreyCodem <deyavijit887@gmail.com>
2026-07-20 22:39:30 +0530

    adding templates to chai app

--- chaiaurDjango/chai/urls.py
+++ chaiaurDjango/chai/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from . import views
+
+#localhost:8000/chai
+#localhost:8000/chai/order
+
 urlpatterns = [
-    path('', views.all_chai, name='all_chai'),
+    path('', views.all_chai, name='all_home'),
+    
 ]
